256f72269606950372a7b18b60e2ea7e0f92bc81
Manoel <manoel.lobo@gmail.com>
2022-08-07 13:09:15 -0300

    refactor: tuples

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ fn main() {
     let width = 10;
     let height = 15;
 
-    let rect_area = area(width, height);
+    let rect_area = area((width, height));
 
     println!("The area of the rectangle is {rect_area}")
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
