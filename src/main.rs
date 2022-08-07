b8acead111657c11d240bc51271e508558235eb4
Manoel <manoel.lobo@gmail.com>
2022-08-07 13:55:12 -0300

    feat: rectangle.can_hold

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -19,4 +23,13 @@ fn main() {
     println!("The area of the rectangle is {}", rectangle.area());
 
     println!("The rectangle is {:?}", rectangle);
+
+    let rectangle2 = Rectangle {
+        width: 8,
+        height: 6,
+    };
+
+    if rectangle.can_hold(&rectangle2) {
+        println!("rectangle can hold rectangle2");
+    }
 }
